de1bd1502cdae95efe0c2a0b59bc3a4525c2ef24
NVai <reyes.s.ign@gmail.com>
2024-07-11 15:24:20 -0400

    fetching highscores api

--- src/components/Scores/Scores.tsx
+++ src/components/Scores/Scores.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Card from "../Card/Card";
 import CardBody from "../CardBody/CardBody";
 import Button from "../Button/Button";
@@ -7,21 +8,54 @@ type ScoresProps = {
   onButtonClick: (buttonName: string) => void;
 };
 
-function Scores(props: ScoresProps) {
-  const { onButtonClick } = props;
+// Define un tipo para la estructura de tus datos
+type Score = {
+  id: number;
+  name: string;
+  score: number;
+};
+
+function Scores({ onButtonClick }: ScoresProps) {
+  const [data, setData] = useState<Score[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/scores")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((data: Score[]) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
-    <>
-      <div className="container mt-5" id="highscores">
-        <Card>
-          <CardBody title="High Scores" description="Falling Words" />
-          <div className="d-flex flex-column align-items-center">
-            <Button name="Start Game" onClick={() => onButtonClick("Game")} />
-            <Button name="Menu" onClick={() => onButtonClick("Menu")} />
-          </div>
-        </Card>
-      </div>
-    </>
+    <div className="container mt-5" id="highscores">
+      <Card>
+        <CardBody title="High Scores" description="Falling Words" />
+        <ul>
+          {data?.map((score) => (
+            <li key={score.id}>{score.name + " " + score.score}</li>
+          ))}
+        </ul>
+        <div className="d-flex flex-column align-items-center">
+          <Button name="Start Game" onClick={() => onButtonClick("Game")} />
+          <Button name="Menu" onClick={() => onButtonClick("Menu")} />
+        </div>
+      </Card>
+    </div>
   );
 }
 
